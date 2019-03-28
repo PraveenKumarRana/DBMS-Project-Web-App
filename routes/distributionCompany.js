0293cfffb4b0fbb066296d91086eba8ecd5905fc
PraveenKumarRana <ced17i019@iiitdm.ac.in>
2019-03-28 23:48:57 +0530

    This adds the search function to the Distribution companies.

--- routes/distributionCompany.js
+++ routes/distributionCompany.js
@@ -42,24 +42,26 @@ router.get("/distcompanylist", function(req,res){
 // post - to /distcompanylist
 router.post("/distcompanylist", function(req,res){
     console.log(req.body.state);
-    var location = req.body.state[0].toUpperCase()+req.body.state.slice(1);
-    console.log(location);
-    const q = `select * from powercompany where location="${location}" or location="${req.body.state}";`;
+    var state = req.body.state[0].toUpperCase()+req.body.state.slice(1);
+    console.log(state);
+    const q = `select * from distributioncompany where state="${state}" or state="${req.body.state}";`;
     connection.query(q, function(error, results, fields){
         if(error){
             console.log(error);
         } else{
-            res.render("power_company/powerData", {result:results});
+            res.render("distribution_company/distributionData", {result:results});
         }
     });
 });
 
-module.exports = router;
+
 router.post("/distcompanylist/new", function(req, res, next){
-    var q = `insert into powercompany values(${req.body.tid},"${req.body.dname}",${req.body.tenure},"${req.body.state}",${req.body.tid});`;
+    var q = `insert into distributioncompany values(${req.body.tid},"${req.body.dname}",${req.body.tenure},"${req.body.state}",${req.body.tid});`;
     console.log(q);
     connection.query(q, function(error, results, fields){
         if(error) throw error;
         res.json(results);
     });
 });
+
+module.exports = router;
