eaf48c9fee6d65fce20ce8257fce6de36ebddd8a
PraveenKumarRana <ced17i019@iiitdm.ac.in>
2019-04-02 12:04:58 +0530

    This adds functionalities for adding, removing and deleting of power company.

--- routes/powerCompanyRoutes.js
+++ routes/powerCompanyRoutes.js
@@ -30,13 +30,67 @@ router.post("/powercompanylist", function(req,res){
     });
 });
 
-router.post("/powercompanylist/new", function(req, res, next){
-    var q = `insert into powercompany values(${req.body.pid},"${req.body.pname}","${req.body.type}",${req.body.totalpower},"${req.body.state}")`;
-    console.log(q);
-    connection.query(q, function(error, results, fields){
-        if(error) throw error;
-        res.json(results);
-    });
+// Adding new distribution company.
+router.get("/powercompany/new", function(req, res, next){
+    res.render("power_company/new",{consumerId:user, admin:admin});
 });
 
+router.post("/powercompany/new", function(req, res, next){
+    if(admin.designation==="designation5"){
+        var q = `insert into powercompany values(${req.body.pid},"${req.body.pname}","${req.body.type}",${req.body.totalpower},"${req.body.state}");`;
+        console.log(q);
+        connection.query(q, function(error, results, fields){
+            if(error) throw error;
+            res.redirect("/powercompanylist");
+        });
+    } else {
+        res.redirect("/adminlogin");
+    }
+});
+
+// Update distribution company.
+router.post("/powercompany/update",function(req,res){
+    console.log("printing from distcompanylist/update route.");
+    var pid = req.body.name;
+    var q = `select * from powercompany where pid=${pid}`;
+    connection.query(q, function(error, results){
+        if(error){
+            console.log("Problem in deleting the value.");
+        } else {
+            res.render("power_company/update",{result:results[0],consumerId:user, admin:admin});
+        }
+    })
+});
+
+router.post("/powercompany/updating", function(req, res){
+    if(admin.designation==="designation5"){
+        var q = `update powercompany set pname="${req.body.pname}",type="${req.body.type}",state="${req.body.state}",totalpower=${req.body.totalpower} where pid=${req.body.pid};`;
+        console.log(q);
+        connection.query(q, function(error, results, fields){
+            if(error) throw error;
+            res.redirect("/powercompanylist");
+        });
+    } else {
+        res.redirect("/adminlogin");
+    }
+});
+
+
+// Deleting transmission company.
+router.post("/powercompany/delete",function(req,res){
+    if(admin.designation==="designation5"){
+    console.log("printing from distcompanylist/delete route.");
+    var pid = req.body.name;
+    var q = `delete from powercompany where pid=${pid}`;
+    connection.query(q, function(error, results){
+        if(error){
+            console.log("Problem in deleting the value.");
+        } else {
+            res.redirect("/powercompanylist");
+        }
+    })
+    } else {
+        res.redirect("/adminlogin");
+    }
+});
 module.exports = router;
