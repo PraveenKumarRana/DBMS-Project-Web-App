317aab14f12e03c55c3bc3ea66a622b597c419b7
PraveenKumarRana <ced17i019@iiitdm.ac.in>
2019-04-02 10:26:45 +0530

    This adds functionalities for adding, removing and deleting of distribution company.

--- routes/distributionCompany.js
+++ routes/distributionCompany.js
@@ -29,21 +29,52 @@ router.post("/distcompanylist", function(req,res){
 });
 
 // Adding new distribution company.
-router.get("/distcompanylist/new", function(req, res, next){
+router.get("/distcompany/new", function(req, res, next){
     res.render("distribution_company/new",{consumerId:user, admin:admin});
 });
 
-router.post("/distcompanylist/new", function(req, res, next){
-    var q = `insert into distributioncompany values(${req.body.did},"${req.body.dname}",${req.body.tenure},"${req.body.state}",${req.body.tid});`;
-    console.log(q);
-    connection.query(q, function(error, results, fields){
-        if(error) throw error;
-        res.redirect("/distcompanylist");
-    });
+router.post("/distcompany/new", function(req, res, next){
+    if(admin.designation==="designation5"){
+        var q = `insert into distributioncompany values(${req.body.did},"${req.body.dname}",${req.body.tenure},"${req.body.state}",${req.body.tid});`;
+        console.log(q);
+        connection.query(q, function(error, results, fields){
+            if(error) throw error;
+            res.redirect("/distcompanylist");
+        });
+    } else {
+        res.redirect("/adminlogin");
+    }
 });
 
+// Update distribution company.
+router.post("/distcompany/update",function(req,res){
+    console.log("printing from distcompanylist/update route.");
+    var did = req.body.name;
+    var q = `select * from distributioncompany where did=${did}`;
+    connection.query(q, function(error, results){
+        if(error){
+            console.log("Problem in deleting the value.");
+        } else {
+            res.render("distribution_company/update",{result:results[0],consumerId:user, admin:admin});
+        }
+    })
+});
+router.post("/distcompany/updating", function(req, res){
+    if(admin.designation==="designation5"){
+        var q = `update distributioncompany set dname="${req.body.dname}",tenure=${req.body.tenure},state="${req.body.state}",tid=${req.body.tid} where did=${req.body.did};`;
+        console.log(q);
+        connection.query(q, function(error, results, fields){
+            if(error) throw error;
+            res.redirect("/distcompanylist");
+        });
+    } else {
+        res.redirect("/adminlogin");
+    }
+})
+
+
 // Deleting distribution company.
-router.post("/distcompanylist/delete",function(req,res){
+router.post("/distcompany/delete",function(req,res){
     console.log("printing from distcompanylist/delete route.");
     var did = req.body.name;
     var q = `delete from distributioncompany where did=${did}`;
@@ -56,11 +87,6 @@ router.post("/distcompanylist/delete",function(req,res){
     })
 });
 
-// Update distribution company.
-router.post("/distcompanylist/update",function(req,res){
-    console.log("printing from distcompanylist/delete route.");
-    console.log(req.body);
-    res.redirect("/distcompanylist");
-});
+
 
 module.exports = router;
