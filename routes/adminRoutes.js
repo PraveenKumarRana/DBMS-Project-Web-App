163f97841792c5c64a024c8928643bacb1ffafc7
PraveenKumarRana <ced17i019@iiitdm.ac.in>
2019-04-04 21:10:02 +0530

    Reject button in newconnection is made functional.

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -32,7 +32,48 @@ router.post("/adminlogin", function(req,res){
 // New connection requests
 //==========================
 router.get("/newconnection/requests", function(req,res){
-    res.render("admin/newconnectionrequest", {consumerId:user, admin:admin})
+    var q=`select * from newconnection;`;
+    connection.query(q, function(error, results){
+        if(error){
+            console.log("Error in getting newconnection information.");
+        } else {
+            console.log(results);
+            res.render("admin/newconnectionrequest", {newconnection:results,consumerId:user, admin:admin});
+        }
+    })
+});
+
+//==========================
+// Approval requests
+//==========================
+router.post("/newconnection/approve", function(req,res){
+    var q=`select * from newconnection where email="${req.body.name}";`;
+    connection.query(q, function(error, results){
+        if(error){
+            console.log("Error in getting newconnection information.");
+        } else {
+            console.log(results);
+            res.render("admin/newconnectionrequest", {newconnection:results,consumerId:user, admin:admin});
+        }
+    })
+});
+
+//==========================
+// Rejection Route
+//==========================
+router.post("/newconnection/reject", function(req,res){
+    if(admin.designation == "designation5"){
+        var q=`delete from newconnection where email="${req.body.name}";`;
+        connection.query(q, function(error, results){
+            if(error){
+                console.log("Error in deleting newconnection information.");
+            } else {
+                res.redirect("/newconnection/requests");
+            }
+        })
+    } else {
+        res.redirect("/adminlogin");
+    }
 });
 
 module.exports = router;
