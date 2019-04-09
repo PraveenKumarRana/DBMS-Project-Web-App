701c2397718743ee110a81422ffe85b830b557e5
PraveenKumarRana <ced17i019@iiitdm.ac.in>
2019-04-09 22:31:52 +0530

    Default port changed to 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const powerCompanyRoutes = require("./routes/powerCompanyRoutes");
 const transCompanyRoutes = require("./routes/transmissionCompany");
 const distCompanyRoutes = require("./routes/distributionCompany");
